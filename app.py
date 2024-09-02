f8f5c090a04e0b2b17c1aeb7f85215edcd6c7e38
tomohiko-urai <84952494+tomohiko-urai@users.noreply.github.com>
2024-09-03 05:26:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ st.sidebar.title("シャインマスカット収穫時期判定アプリ-ai-app1
 st.sidebar.write("画像認識モデルを使ってシャインマスカットの収穫時期の判定をします。")
 
 st.sidebar.write("")
-col1,col2 = st.columns(2)
+#col1,col2 = st.columns(2)
 
-#img_source = st.sidebar.radio("画像のソースを選択してください。",
-#                              ("画像をアップロード", "カメラで撮影"))
-#if img_source == "画像をアップロード":
-#    img_file = st.sidebar.file_uploader("画像を選択してください。", type=["png", "jpg", "JPG"])
-#elif img_source == "カメラで撮影":
-with col1:    
+img_source = st.sidebar.radio("画像のソースを選択してください。",
+                              ("画像をアップロード", "カメラで撮影"))
+if img_source == "画像をアップロード":
+    img_file = st.sidebar.file_uploader("画像を選択してください。", type=["png", "jpg", "JPG"])
+elif img_source == "カメラで撮影":
+#with col1:    
     img_file = st.camera_input("カメラで撮影")
 
 #with col2:
@@ -45,7 +45,7 @@ if img_file is not None:
         
     
         # 結果の表示
-    with col2:       
+    #with col2:       
         #st.subheader("判定結果")
         st.subheader("判定結果")
         st.image(annotated_frame, caption='出力画像', width=280) 
